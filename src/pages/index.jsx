233d0d0a39eeb873cd81462c1c43930c880baeaa
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2023-03-27 15:48:53 +0700

    ok

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ import Head from 'next/head'
 import Header from "../components/Header"
 import { useRouter } from 'next/router';
 import logo from '../../public/assets/img/pdipm.png';
+import Navbarku from "../components/Navbar";
 
 
 export default function Home() {
@@ -31,38 +32,7 @@ export default function Home() {
           Home - SIA PD IPM Pekanbaru
         </title>
       </Head>
-      <nav
-        className="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3"
-        id="mainNav">
-        <div className="container">
-          <Link className="navbar-brand d-flex align-items-center" href="/"><span>
-            <Image
-              src={logo}
-              className="img-fluid"
-              alt="logo pd ipm"
-              width="50"
-            />
-            SIA PD IPM</span></Link><button
-              data-bs-toggle="collapse"
-              className="navbar-toggler"
-              data-bs-target="#navcol-1">
-            <span className="visually-hidden">Toggle navigation</span>
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navcol-1">
-            <ul className="navbar-nav mx-auto">
-              {navigation.map((item) => (
-                <li key={item.name} className="nav-item">
-                  <Link className={`${item.classLink}`} href={item.Link}>{item.name}</Link>
-                </li>
-              ))}
-
-            </ul>
-              <Link className="btn btn-primary shadow" role="button" href="signup.html"
-            >Sign up</Link>
-          </div>
-        </div>
-      </nav>
+    <Navbarku/>
       <Header />
 
 
