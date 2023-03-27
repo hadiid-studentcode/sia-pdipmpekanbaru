f2e7f7d5fa128cb620494d42f147edb8b1b5e7de
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2023-03-27 15:07:31 +0700

    ok

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,3 @@
-import Image from 'next/image';
-import markEmail from "../../public/assets/img/icon/mark_email_unread_FILL0_wght400_GRAD0_opsz48.svg";
-import forwardToInbox from "../../public/assets/img/icon/forward_to_inbox_FILL0_wght400_GRAD0_opsz48.svg";
-import contactEmail from "../../public/assets/img/icon/contact_mail_FILL0_wght400_GRAD0_opsz48.svg";
-import presentation from "../../public/assets/img/illustrations/presentation.svg"
-import webDevelopment from "../../public/assets/img/illustrations/web-development.svg"
-import Link from "next/link";
 import Head from 'next/head'
 import Header from "../components/Header"
 
